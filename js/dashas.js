74ef54ae54b162b05cc28fa752349164a5ba549f
Ravi Annaswamy <ravi.annaswamy@gmail.com>
2025-04-26 21:03:53 -0400

    Update dashas.js

--- js/dashas.js
+++ js/dashas.js
@@ -132,9 +132,9 @@ function buildDashaData(birthJD, natalDashaIndex) {
 function makeDashaTables(dashas, birthJD, offsetNow) {
   let currIdx = findCurrentDashaIndex(dashas, offsetNow);
 
-  let dashaTable = '<h4>தசா வரிசை</h4>' +
+  let dashaTable = '<h4>தசை வரிசை</h4>' +
                    '<table><thead><tr>' +
-                   '<th>மஹா தசா</th>' +
+                   '<th>மஹாதசை</th>' +
                    '<th>தொடக்கம்</th>' +
                    '<th>முடிவு</th>' +
                    '</tr></thead><tbody>';
@@ -230,7 +230,7 @@ function showBhuktis(mahadashaIndex) {
     }
   }
 
-  let bhuktiTable = '<h4>' + currentDashaLord + ' தசாபுக்தி வரிசை</h4>' +
+  let bhuktiTable = '<h4>' + currentDashaLord + ' தசை புக்தி வரிசை</h4>' +
                     '<table><thead><tr>' +
                     '<th>புக்தி</th>' +
                     '<th>தொடக்கம்</th>' +
